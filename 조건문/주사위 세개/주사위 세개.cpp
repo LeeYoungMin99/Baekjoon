74bb01a235083553dfc644484a2b071c37b71a87
LeeYM <sixk1999@gmail.com>
2022-04-16 16:14:46 +0900

    주사위 세개

---
+++ 조건문/주사위 세개/주사위 세개.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int a, b, c;
+int Count = 0;
+int Max;
+int Equal;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	cin >> a >> b >> c;
+
+	if (a == b || a == c)
+	{
+		++Count;
+
+		Equal = a;
+	}
+
+	if (b == c)
+	{
+		++Count;
+
+		Equal = b;
+	}
+
+	Max = max(a, b);
+	Max = max(Max, c);
+
+	if		(2 == Count) cout << 1000 * Equal + 10000;
+	else if (1 == Count) cout << 100  * Equal + 1000;
+	else				 cout << 100  * Max;
+}
