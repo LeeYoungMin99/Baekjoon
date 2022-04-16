b12f37b577fd83baeecb0672efd203ed9ceb361a
LeeYM <sixk1999@gmail.com>
2022-04-16 14:22:14 +0900

    나머지

---
+++ 나머지/나머지.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int a, b, c;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	cin >> a >> b >> c;
+
+	cout << (a + b) % c << endl;
+	cout << ((a % c) + (b % c)) % c << endl;
+	cout << (a * b) % c << endl;
+	cout << ((a % c) * (b % c)) % c;
+}
