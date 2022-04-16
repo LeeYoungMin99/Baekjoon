1835b3e8e8c24ba1f175a89978e75f0ad1d42692
LeeYM <sixk1999@gmail.com>
2022-04-16 14:09:48 +0900

    A / B

---
+++ A 나누기 B/A 나누기 B.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double a, b;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	cin >> a >> b;
+
+	cout.precision(13);
+
+	cout << a / b;
+}
