6cb7405d21e0148f0b3983fd7e4c56e42f5bace1
LeeYM <sixk1999@gmail.com>
2022-04-16 16:31:40 +0900

    합

---
+++ 반복문/합/합.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int a;
+int result;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	cin >> a;
+
+	for (int i = 1; i <= a; ++i)
+	{
+		result += i;
+	}
+
+	cout << result;
+}
