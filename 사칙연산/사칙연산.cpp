2a1ec15d5a4e28a4d579d3fe17a61c5a4609c13f
LeeYM <sixk1999@gmail.com>
2022-04-16 14:12:30 +0900

    사칙연산

---
+++ 사칙연산/사칙연산.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int a, b;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	cin >> a >> b;
+
+	cout << a + b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+	cout << a / b << endl;
+	cout << a % b << endl;
+}
