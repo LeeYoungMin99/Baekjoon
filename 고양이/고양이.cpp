a6dac21b1abd56489ed8a4eaf8f1b859679c6be9
LeeYM <sixk1999@gmail.com>
2022-04-16 13:54:39 +0900

    고양이

---
+++ 고양이/고양이.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	cout << "\\    /\\\n";
+	cout << " )  ( ')\n";
+	cout << "(  /  )\n";
+	cout << " \\(__)|\n";
+}
