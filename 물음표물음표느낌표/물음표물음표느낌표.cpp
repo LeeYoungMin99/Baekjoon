daee048fe25b15867ad358b3d6ce74e882871eef
LeeYM <sixk1999@gmail.com>
2022-04-16 14:15:12 +0900

    ??!

---
+++ 물음표물음표느낌표/물음표물음표느낌표.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+string name;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	cin >> name;
+
+	cout << name << "??!";
+}
