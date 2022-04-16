3757c91cb434ee0af41e8fa50c7025f30b6d8cdd
LeeYM <sixk1999@gmail.com>
2022-04-16 15:47:17 +0900

    오븐 시계

---
+++ 조건문/오븐 시계/오븐 시계.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int h, m, t;
+int result;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	cin >> h >> m;
+	cin >> t;
+
+	result = (h * 60) + m + t;
+
+	h = result / 60;
+	m = result % 60;
+
+	h = (24 > h) ? h : h - 24;
+
+	cout << h << " " << m;
+}
