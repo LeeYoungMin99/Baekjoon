44fbf4c1639725b4c7d1ed9b48eea45c33d5526f
LeeYM <sixk1999@gmail.com>
2022-04-16 15:00:01 +0900

    시험성적

---
+++ 조건문/시험성적/시험성적.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int a;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	cin >> a;
+
+	if		(90 <= a) cout << "A";
+	else if (80 <= a) cout << "B";
+	else if (70 <= a) cout << "C";
+	else if (60 <= a) cout << "D";
+	else			  cout << "F";
+}
