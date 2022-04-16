545ab371dc3748ff3685236c7a8e969bac6894ec
LeeYM <sixk1999@gmail.com>
2022-04-16 16:58:41 +0900

    별 찍기 - 1

---
+++ 반복문/별 찍기 - 1/별 찍기 - 1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int n;
+int a, b;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	cin >> n;
+
+	for (int i = 1; i <= n; ++i)
+	{
+		for (int j = 0; j < i; ++j)
+		{
+			cout << "*";
+		}
+
+		cout << "\n";
+	}
+}
