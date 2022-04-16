0007a8594b7f0c7c768e8c536865a0c2d1225fac
LeeYM <sixk1999@gmail.com>
2022-04-16 14:55:26 +0900

    두 수 비교하기

---
+++ 조건문/두 수 비교하기/두 수 비교하기.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int a, b;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	cin >> a >> b;
+
+	if (a > b)  cout << ">";
+	if (a < b)  cout << "<";
+	if (a == b) cout << "==";
+}
