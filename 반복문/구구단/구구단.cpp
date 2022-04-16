1e03aa757222c169e5c8b22bc23ac03767068d30
LeeYM <sixk1999@gmail.com>
2022-04-16 16:22:21 +0900

    구구단

---
+++ 반복문/구구단/구구단.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int a;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	cin >> a;
+
+	for (int i = 1; i < 10; ++i)
+	{
+		cout << a << " * " << i << " = " << a * i << endl;
+	}
+}
