3fba691a24290119c0b32003cfd5b827c5ea6c8d
LeeYM <sixk1999@gmail.com>
2022-04-16 16:48:10 +0900

    기찍 N

---
+++ 반복문/기찍 N/기찍 N.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int n;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	cin >> n;
+
+	for (int i = n; i > 0; --i)
+	{
+		cout << i << "\n";
+	}
+}
