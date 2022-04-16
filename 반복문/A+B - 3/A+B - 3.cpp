876e059f710351e63237d390a68062f2880abd59
LeeYM <sixk1999@gmail.com>
2022-04-16 16:24:41 +0900

    A + B - 3

---
+++ 반복문/A+B - 3/A+B - 3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int t;
+int a, b;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	cin >> t;
+
+	for (int i = 0; i < t; ++i)
+	{
+		cin >> a >> b;
+
+		cout << a + b << endl;
+	}
+}
