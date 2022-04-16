ed7003d3d38dfe6a592e33aff73034fc272d07cd
LeeYM <sixk1999@gmail.com>
2022-04-16 17:05:16 +0900

    X보다 작은 수

---
+++ 반복문/X보다 작은 수/X보다 작은 수.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int n;
+int x;
+int a;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	cin >> n >> x;
+
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> a;
+
+		if (a < x) cout << a << " ";
+	}
+}
