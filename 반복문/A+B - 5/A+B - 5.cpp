cbf6facd223dfa81b8fbd16c220beb92bd900540
LeeYM <sixk1999@gmail.com>
2022-04-16 17:10:52 +0900

    A + B - 5

---
+++ 반복문/A+B - 5/A+B - 5.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int a, b;
+int result;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	while (true)
+	{
+		cin >> a >> b;
+
+		result = a + b;
+
+		if (result == 0) break;
+
+		cout << result << "\n";
+	}
+}
