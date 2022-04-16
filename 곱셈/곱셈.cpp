74aaee44b43c24ef27a2bc4b3082e14360b5bf32
LeeYM <sixk1999@gmail.com>
2022-04-16 14:33:43 +0900

    곱셈

---
+++ 곱셈/곱셈.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int a, b;
+int result;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	cin >> a >> b;
+
+	cout << a * (b % 10) << endl;
+	cout << a * ((b / 10) % 10) << endl;
+	cout << a * (b / 100) << endl;
+	cout << a * b << endl;
+}
