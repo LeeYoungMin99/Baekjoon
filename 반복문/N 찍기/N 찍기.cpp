fa9d5c53c7a80a556e6790c1d94bf2c9bd14dcd2
LeeYM <sixk1999@gmail.com>
2022-04-16 16:46:50 +0900

    N 찍기

---
+++ 반복문/N 찍기/N 찍기.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int n;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	cin >> n;
+
+	for (int i = 1; i <= n; ++i)
+	{
+		cout << i << "\n";
+	}
+}
