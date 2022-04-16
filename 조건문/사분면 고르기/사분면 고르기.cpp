b6219d40defd55f25af1fe8565fd79511b335cc8
LeeYM <sixk1999@gmail.com>
2022-04-16 15:20:24 +0900

    사분면 고르기

---
+++ 조건문/사분면 고르기/사분면 고르기.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int a, b;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	cin >> a >> b;
+
+	if		(a > 0 && b > 0) cout << 1;
+	else if (a < 0 && b > 0) cout << 2;
+	else if (a < 0 && b < 0) cout << 3;
+	else					 cout << 4;
+}
