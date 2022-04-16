0235f0d8da269e3d6ed5a6f1eaab8b180d4417f3
LeeYM <sixk1999@gmail.com>
2022-04-16 15:37:30 +0900

    알람 시계

---
+++ 조건문/알람 시계/알람 시계.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int h, m;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	cin >> h >> m;
+
+	m -= 45;
+
+	if (0 > m)
+	{
+		m += 60;
+
+		h = (1 <= h) ? --h : 23;
+	}
+
+	cout << h << " " << m;
+}
