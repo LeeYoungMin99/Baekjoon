29379bcd8384ccebddaf7d721426a6abccb62f7f
LeeYM <sixk1999@gmail.com>
2022-04-16 16:54:57 +0900

    A + B - 8

---
+++ 반복문/A+B - 8/A+B - 8.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int t;
+int a, b;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	cin >> t;
+
+	for (int i = 1; i <= t; ++i)
+	{
+		cin >> a >> b;
+
+		cout << "Case #" << i << ": " << a << " + " << b << " = " << a + b << "\n";
+	}
+}
