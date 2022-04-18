27da9bbe890d147c0772dcb3d53b37e0627e11a8
LeeYM <sixk1999@gmail.com>
2022-04-18 22:39:38 +0900

    블랙잭, 분해 합

---
+++ 브루트 포스/분해합/분해합.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int x, y;
+int Min;
+int Sum;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	cin >> n;
+
+	y = x = n;
+
+	while (0 < y)
+	{
+		x -= 9;
+		y /= 10;
+	}
+
+	while (true)
+	{
+		Sum = y = x;
+
+		while (0 < y)
+		{
+			Sum += y % 10;
+			y /= 10;
+		}
+
+		if (Sum == n)
+		{
+			Min = x;
+
+			break;
+		}
+
+		if (n == x) break;
+
+		++x;
+	}
+
+	cout << Min;
+}
---
+++ 브루트 포스/블랙잭/블랙잭.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+int card[101];
+int Max = 0;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	cin >> n >> m;
+
+	for (int i = 1; i <= n; ++i)
+	{
+		cin >> card[i];
+	}
+
+	sort(card, card + n);
+
+	for (int i = 1; i <= n - 2; ++i)
+	{
+		for (int j = 1 + i; j <= n - 1; ++j)
+		{
+			for (int k = 1 + j; k <= n; ++k)
+			{
+				int sum = card[i] + card[j] + card[k];
+				if (m < sum) break;
+				if (Max < sum) Max = sum;
+			}
+		}
+	}
+
+	cout << Max;
+}
