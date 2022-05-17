b48d328a46cdff1e6364150a1248cdc0cdbce809
LeeYM <sixk1999@gmail.com>
2022-05-17 20:40:19 +0900

    입국심사

--- Study/H-Index/H-Index.cpp
+++ Study/H-Index/H-Index.cpp
@@ -1,26 +1,24 @@
 ﻿#include <string>
 #include <vector>
-#include <iostream>
 #include <algorithm>
 
 using namespace std;
 
 int solution(vector<int> citations)
 {
-    sort(citations.begin(), citations.end());
+	sort(citations.begin(), citations.end());
 
-    size_t value = citations[citations.size() - 1];
-    size_t size = citations.size();
+	size_t size = citations.size();
+	size_t value = citations[size - 1];
 
-    vector<int>::iterator begin = citations.begin();
-    vector<int>::iterator end = citations.end();
+	vector<int>::iterator begin = citations.begin();
+	vector<int>::iterator end = citations.end();
+	for (size_t i = 0; i <= value; ++i)
+	{
+		size_t count = size - (upper_bound(begin, end, i) - begin);
 
-    for (size_t i = 0; i <= value; ++i)
-    {
-        size_t count = size - (upper_bound(begin, end, i) - begin);
+		if (count > i) continue;
 
-        if (count > i) continue;
-
-        return i;
-    }
+		return i;
+	}
 }
---
+++ Study/입국심사/입국심사.cpp
@@ -0,0 +1,110 @@
+﻿#include <vector>
+#include <queue>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+long long solution(int n, vector<int> times)
+{
+	long long answer = 0;
+
+	size_t size = times.size();
+
+	sort(times.begin(), times.end());
+
+	if (n <= size)
+	{
+		answer = times[n - 1];
+
+		return answer;
+	}
+
+	long long min = 1;
+	long long max = times[0] * n;
+	long long mid = (min + max) / 2;
+
+	while (min <= max)
+	{
+		long long count = 0;
+		for (size_t i = 0; i < size; ++i)
+		{
+			count += mid / times[i];
+		}
+
+		if (n > count)
+		{
+			min = mid + 1;
+		}
+		else
+		{
+			max = mid - 1;
+		}
+
+		mid = (min + max) / 2;
+	}
+
+	answer = min;
+
+	return answer;
+}
+
+struct compare
+{
+	bool operator()(pair<long long, size_t> l, pair<long long, size_t> r)
+	{
+		return l.first > r.first;
+	}
+};
+
+long long solution2(int n, vector<int> times)
+{
+	long long answer = 0;
+
+	size_t size = times.size();
+
+	sort(times.begin(), times.end());
+
+	if (n <= size)
+	{
+		answer = times[n - 1];
+
+		return answer;
+	}
+
+	vector<long long> llvec;
+	llvec.reserve(n);
+
+	priority_queue<pair<long long, size_t>, vector<pair<long long, size_t>>, compare> pq;
+
+	for (size_t i = 0; i < size; ++i)
+	{
+		pq.push(make_pair(times[i], i));
+	}
+
+	pair<long long, size_t> top;
+	size = 0;
+	while (n >= size)
+	{
+		top = pq.top();
+		pq.pop();
+
+		llvec.push_back(top.first);
+		++size;
+
+		top.first += times[top.second];
+
+		pq.push(top);
+	}
+
+	answer = llvec[n - 1];
+
+	return answer;
+}
+
+
+
+int main()
+{
+	cout << solution(3, { 1,99,99 });
+}
