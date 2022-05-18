ea4942b1223bc25fb58c242055082ecb5e08fb58
LeeYM <sixk1999@gmail.com>
2022-05-18 21:49:24 +0900

    좌표 압축

---
+++ 정렬/좌표 압축/좌표 압축.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+vector<long long> coordinates;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	cin >> n;
+
+	long long coordinate;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> coordinate;
+
+		coordinates.push_back(coordinate);
+	}
+
+	vector<long long> copyCoordinates = coordinates;
+
+	sort(coordinates.begin(), coordinates.end());
+	coordinates.erase(unique(coordinates.begin(), coordinates.end()), coordinates.end());
+
+	vector<long long>::iterator begin = coordinates.begin();
+	vector<long long>::iterator end = coordinates.end();
+	for (int i = 0; i < n; ++i)
+	{
+		cout << lower_bound(begin, end, copyCoordinates[i]) - begin << ' ';
+	}
+}
