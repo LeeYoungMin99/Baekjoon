7cf4ba9f893589a5b58abd3e20859c24b293850c
LeeYM <sixk1999@gmail.com>
2022-05-23 00:44:33 +0900

    숫자 카드 2

---
+++ 집합과 맵/숫자 카드 - 2/숫자 카드 - 2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int cards[20000001];
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+
+	cin >> n;
+
+	int number;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> number;
+
+		++cards[number + 10000000];
+	}
+
+	cin >> n;
+
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> number;
+
+		cout << cards[number + 10000000] << ' ';
+	}
+}
