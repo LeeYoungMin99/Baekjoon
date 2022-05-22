a58080a65203e18f8a014c4a99ccee46443d0d41
LeeYM <sixk1999@gmail.com>
2022-05-22 19:30:13 +0900

    숫자 카드

---
+++ 집합과 맵/숫자 카드/숫자 카드.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+bool cards[20000001];
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+
+	cin >> n;
+
+	int number;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> number;
+
+		cards[number + 10000000] = true;
+	}
+
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> number;
+
+		if (true == cards[number + 10000000]) cout << "1 ";
+		else								  cout << "0 ";
+	}
+}
