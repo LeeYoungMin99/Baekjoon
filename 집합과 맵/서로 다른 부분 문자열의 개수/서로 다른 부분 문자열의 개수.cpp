3fdad3f2aaffeab074c90d3f145b4016015e964e
LeeYM <sixk1999@gmail.com>
2022-05-24 14:32:38 +0900

    서로 다른 부분 문자열의 개수

---
+++ 집합과 맵/서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <algorithm>
+
+using namespace std;
+
+string s;
+unordered_set<string> us;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+
+	cin >> s;
+
+	size_t length = s.length();
+	for (size_t i = 0; i < length; ++i)
+	{
+		for (size_t j = 0; j < length - i; ++j)
+		{
+			string key = "";
+
+			key.reserve(i + 1);
+
+			for (size_t k = j; k <= j + i; ++k)
+			{
+				key += s[k];
+			}
+
+			us.insert(key);
+		}
+	}
+
+	cout << us.size();
+}
