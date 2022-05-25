8aa3d03f819cc8d4bc6e4d9256a5915a56f66d5d
LeeYM <sixk1999@gmail.com>
2022-05-25 16:01:07 +0900

    문자열 집합

---
+++ 집합과 맵/문자열 집합/문자열 집합.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+unordered_map<string, bool> us;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+
+	cin >> n >> m;
+
+	string str;
+	size_t length;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> str;
+
+		us[str] = true;
+	}
+
+	int count = 0;
+	for (int i = 0; i < m; ++i)
+	{
+		cin >> str;
+
+		if (us[str])
+		{
+			++count;
+		}
+	}
+
+	cout << count;
+}
