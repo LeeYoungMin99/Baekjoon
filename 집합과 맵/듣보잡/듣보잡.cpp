008e897dd8c530a6a3c39793dddce25cabb4c230
LeeYM <sixk1999@gmail.com>
2022-05-29 21:45:07 +0900

    듣보잡

---
+++ 집합과 맵/듣보잡/듣보잡.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+unordered_map<string, string> um;
+vector<string> notHearAndLook;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+
+	cin >> n >> m;
+
+	string notHear;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> notHear;
+
+		um[notHear] = notHear;
+	}
+
+	string notLook;
+	for (int i = 0; i < m; ++i)
+	{
+		cin >> notLook;
+
+		if (notLook != um[notLook]) continue;
+
+		notHearAndLook.push_back(um[notLook]);
+	}
+
+	sort(notHearAndLook.begin(), notHearAndLook.end());
+
+	size_t size = notHearAndLook.size();
+	cout << size << '\n';
+
+	for (size_t i = 0; i < size; ++i)
+	{
+		cout << notHearAndLook[i] << '\n';
+	}
+}
