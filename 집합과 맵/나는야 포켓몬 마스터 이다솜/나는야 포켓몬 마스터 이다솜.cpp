36678019f2a5c49369c5c5d02a137d1ad47cb70c
LeeYM <sixk1999@gmail.com>
2022-06-11 00:27:01 +0900

    나는야 포켓몬 마스터 이다솜

---
+++ 집합과 맵/나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+unordered_map<string, string> poketmons;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+
+	cin >> n >> m;
+
+	string name;
+	for (int i = 1; i <= n; ++i)
+	{
+		cin >> name;
+
+		string numberToString = to_string(i);
+
+		poketmons[name] = numberToString;
+		poketmons[numberToString] = name;
+	}
+
+	string quiz;
+	for (int i = 0; i < m; ++i)
+	{
+		cin >> quiz;
+
+		cout << poketmons[quiz] << '\n';
+	}
+}
