aaa72d3b8d3649280fbd7ad9202155e40f2ca9ea
LeeYM <sixk1999@gmail.com>
2022-06-12 15:11:50 +0900

    좌표 정렬하기 - 1

---
+++ 정렬/좌표 정렬하기 - 1/좌표 정렬하기 - 1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+pair<int, int> coordinates[100001];
+
+struct compare
+{
+	bool operator()(pair<int, int> l, pair<int, int> r)
+	{
+		if (l.first != r.first)	return l.first < r.first;
+		else					return l.second < r.second;
+	}
+};
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+
+	cin >> n;
+
+	int x, y;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> x >> y;
+
+		coordinates[i] = make_pair(x, y);
+	}
+
+	sort(coordinates, coordinates + n);
+
+	for (int i = 0; i < n; ++i)
+	{
+		cout << coordinates[i].first << " " << coordinates[i].second << '\n';
+	}
+}
