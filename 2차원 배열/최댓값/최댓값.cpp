381bb2cbf205f22a71ffc436b7b228bfd53145ce
LeeYM <sixk1999@gmail.com>
2023-02-13 22:43:11 +0900

    최댓값

---
+++ 2차원 배열/최댓값/최댓값.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+
+	int max = -1, x, y;
+
+	for (int i = 1; i < 10; ++i)
+	{
+		for (int j = 1; j < 10; ++j)
+		{
+			int a;
+			cin >> a;
+			if (max >= a)
+				continue;
+
+			max = a;
+			x = i;
+			y = j;
+		}
+	}
+
+	cout << max << '\n' << x << ' ' << y;
+}
