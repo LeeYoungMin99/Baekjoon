f2501584d085d2007018c1daf1ce7ad2b28a9f6c
LeeYM <sixk1999@gmail.com>
2023-02-13 21:33:02 +0900

    영수증

---
+++ 반복문/영수증/영수증.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int x = 0;
+int n = 0;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+
+	cin >> x;
+	cin >> n;
+
+	int value = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		int a, b;
+		cin >> a >> b;
+
+		value += a * b;
+	}
+
+	string result = (value == x) ? "Yes" : "No";
+
+	cout << result;
+}
