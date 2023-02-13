03a429dcd9244eb3af97d776fd4d5492016b77d9
LeeYM <sixk1999@gmail.com>
2023-02-13 22:37:02 +0900

    행렬 덧셈

---
+++ 2차원 배열/행렬 덧셈/행렬 덧셈.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int n, m;
+
+int arr[101][101] = { 0 };
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+
+	cin >> n >> m;
+
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < m; ++j)
+		{
+			cin >> arr[i][j];
+		}
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < m; ++j)
+		{
+			int x;
+			cin >> x;
+			cout << arr[i][j] + x << ' ';
+		}
+
+		cout << endl;
+	}
+}
