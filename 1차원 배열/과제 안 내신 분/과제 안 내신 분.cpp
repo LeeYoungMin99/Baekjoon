2fa6b0c712383645b7503f92b5f6f5bdf46cacb3
LeeYM <sixk1999@gmail.com>
2023-02-13 21:46:32 +0900

    과제 안 내신 분..?

---
+++ 1차원 배열/과제 안 내신 분/과제 안 내신 분.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int values[31] = { 0 };
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+
+	for (int i = 0; i < 28; ++i)
+	{
+		int x;
+		cin >> x;
+
+		values[x] = 1;
+	}
+
+	for (int i = 1, iCount = 31; i < iCount; ++i)
+	{
+		if (values[i] == 0)
+			cout << i << endl;
+	}
+}
