2f6642c603737ca6043dd609404197af37b8c72e
LeeYM <sixk1999@gmail.com>
2023-02-13 21:13:34 +0900

    킹, 퀸, 룩, 비숍, 나이트, 폰

---
+++ 입출력과 사칙연산/킹, 퀸, 룩, 비숍, 나이트, 폰/킹, 퀸, 룩, 비숍, 나이트, 폰.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int counts[6] = { 1,1,2,2,2,8 };
+int inputCounts[6] = { 0,0,0,0,0,0 };
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+
+	for (int i = 0; i < 6; ++i)
+		cin >> inputCounts[i];
+
+	for (int i = 0; i < 6; ++i)
+		cout << counts[i] - inputCounts[i] << ' ';
+}
