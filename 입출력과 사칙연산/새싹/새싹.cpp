76ad1eabcb36571c5f67f5665481d9424e849fff
LeeYM <sixk1999@gmail.com>
2023-02-13 21:26:43 +0900

    새싹

---
+++ 입출력과 사칙연산/새싹/새싹.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+
+	cout << "         ,r\'\"7\n";
+	cout << "r`-_   ,'  ,/\n";
+	cout << " \\. \". L_r'\n";
+	cout << "   `~\\/\n";
+	cout << "      |\n";
+	cout << "      |\n";
+}
