943de4415ef848f545bd390ba370405662a1f5ff
LeeYM <sixk1999@gmail.com>
2023-02-13 22:30:50 +0900

    부녀회장이 될테야

--- 기본 수학 1/부녀회장이 될테야/부녀회장이 될테야.cpp
+++ 기본 수학 1/부녀회장이 될테야/부녀회장이 될테야.cpp
@@ -6,20 +6,33 @@ int t;
 int k, n;
 int result = 1;
 
+int arr[16][16] = { 0 };
+
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cout.tie(NULL);
 	cin.tie(NULL);
 
-	cin >> t;
+	for (int i = 1; i < 15; ++i)
+		arr[0][i] = i;
 
+	for (int x = 1, i = 0; x < 15; ++x, ++i)
+	{
+		for (int y = 1, j = 0; y < 15; ++y, ++j)
+		{
+			arr[x][y] = arr[x][j] + arr[i][y];
+		}
+	}
+
+	cin >> t;
+	
 	for (int i = 0; i < t; ++i)
 	{
 		cin >> k;
 		cin >> n;
-
-
+	
+		cout << arr[k][n] << endl;
 	}
 
 }
