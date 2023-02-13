c6b9a2f071eec678b75ba630e430889a5dd3e9d0
LeeYM <sixk1999@gmail.com>
2023-02-13 21:38:06 +0900

    개수 세기

---
+++ 1차원 배열/개수 세기/개수 세기.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int values[201] = { 0 };
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+	{
+		int x;
+		cin >> x;
+
+		++values[x + 100];
+	}
+
+	int v;
+	cin >> v;
+
+	int result = values[v + 100];
+
+	cout << result;
+}
