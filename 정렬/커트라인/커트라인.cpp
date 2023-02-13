359f02e44ac2f00bfdb15d1666777213240c4d68
LeeYM <sixk1999@gmail.com>
2023-02-13 23:29:36 +0900

    커트라인

---
+++ 정렬/커트라인/커트라인.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, k;
+vector<int> vec;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+
+	cin >> n >> k;
+	for (int i = 0; i < n; ++i)
+	{
+		int x;
+		cin >> x;
+
+		vec.push_back(x);
+	}
+
+	sort(vec.begin(), vec.end());
+	cout << vec[n - k];
+}
