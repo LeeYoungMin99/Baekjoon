1ae9f7414c44e24ee814b66c4dedb069379e3b16
LeeYM <sixk1999@gmail.com>
2023-02-13 23:21:35 +0900

    대표값2

---
+++ 정렬/대표값2/대표값2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> vec;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+
+	int average = 0;
+	for (int i = 0; i < 5; ++i)
+	{
+		int x;
+		cin >> x;
+
+		vec.push_back(x);
+		average += x;
+	}
+
+	sort(vec.begin(), vec.end());
+	size_t size = vec.size();
+	cout << average / size << '\n' << vec[size / 2];
+}
