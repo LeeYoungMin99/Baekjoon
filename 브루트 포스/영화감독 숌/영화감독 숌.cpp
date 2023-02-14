e3b1be6b951a4f758c6d3993abfab2d165004078
LeeYM <sixk1999@gmail.com>
2023-02-14 22:07:14 +0900

    영화감독 숌

---
+++ 브루트 포스/영화감독 숌/영화감독 숌.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+
+	cin >> n;
+
+	int count = 1;
+	int value = 666;
+	while (count < n)
+	{
+		++value;
+
+		string str = to_string(value);
+		if (str.find("666") != std::string::npos)
+			++count;
+	}
+
+	cout << value;
+}
