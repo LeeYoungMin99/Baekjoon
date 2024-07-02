45fae92b8395a360d4db2b191fcf9cf7ee8c59a5
20Minutes <sixk1999@gmail.com>
2024-07-02 13:28:20 +0900

    20240702

---
+++ 001934.최소공배수/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System;
+using System.Text;
+
+var sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+var sw = new StreamWriter(Console.OpenStandardOutput());
+
+var t = int.Parse(sr.ReadLine()!);
+
+for (int i = 0; i < t; ++i)
+{
+    var sa = sr.ReadLine()!.Split();
+    var a = int.Parse(sa[0]);
+    var b = int.Parse(sa[1]);
+
+    for (int j = 1, k = 1; ;)
+    {
+        var c = a * j;
+        var d = b * k;
+
+        if (c == d)
+        {
+            sw.WriteLine(c);
+            break;
+        }
+
+        if (c < d) ++j;
+        else ++k;
+    }
+}
+
+sw.Close();
---
+++ 024313.알고리즘 수업 - 점근적 표기 1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System;
+using System.Text;
+
+var sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+var sw = new StreamWriter(Console.OpenStandardOutput());
+
+var sa = sr.ReadLine()!.Split();
+
+var a1 = int.Parse(sa[0]);
+var a0 = int.Parse(sa[1]);
+
+var c = int.Parse(sr.ReadLine()!);
+var n = int.Parse(sr.ReadLine()!);
+
+sw.WriteLine(((a1 * n) + a0) <= (c * n) && a1 <= c ? 1 : 0);
+sw.Close();
