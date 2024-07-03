0fc82c37b145dc9058d2f1052f328da5c72028c8
20Minutes <sixk1999@gmail.com>
2024-07-03 13:32:41 +0900

    20240703

---
+++ 001735.분수 합/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System;
+using System.Text;
+
+var sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+var sw = new StreamWriter(Console.OpenStandardOutput());
+
+var sa1 = sr.ReadLine()!.Split();
+var sa2 = sr.ReadLine()!.Split();
+var a = long.Parse(sa1[0]);
+var b = long.Parse(sa1[1]);
+var c = long.Parse(sa2[0]);
+var d = long.Parse(sa2[1]);
+
+var e = GCD(b, d);
+var f = b / e * d;
+var g = a * (f / b) + c * (f / d);
+var h = GCD(f, g);
+
+sw.WriteLine($"{g / h} {f / h}");
+sw.Close();
+
+
+
+static long GCD(long a, long b)
+{
+    if (a < b) return GCD(b, a);
+    if (b == 0) return a;
+    return GCD(b, a % b);
+}
---
+++ 013241.최소공배수/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System;
+using System.Text;
+
+var sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+var sw = new StreamWriter(Console.OpenStandardOutput());
+
+var sa = sr.ReadLine()!.Split();
+var a = long.Parse(sa[0]);
+var b = long.Parse(sa[1]);
+
+var c = a;
+var d = b;
+while (true)
+{
+    if (d == 0) break;
+    if (c < d) (c, d) = (d, c);
+    (c, d) = (d, c % d);
+}
+sw.WriteLine(a / c * b);
+
+sw.Close();
